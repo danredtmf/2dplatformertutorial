2824d4a456d7c97d5e8c19c5f118ccfc8c5100ab
DanRedTMF <57467826+danredtmf@users.noreply.github.com>
2025-02-25 01:04:41 +0700

    sync changes

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -40,6 +40,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	# TODO Баг, игрок продолжает двигается после нажатия drop
 	if not is_busy:
 		# Прыжок
 		if Input.is_action_just_pressed("jump") and is_on_floor():
